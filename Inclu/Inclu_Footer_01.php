cecdfc6ca3753d6d1146702d6471ee4d20dcdda7
juanbarrospazos@hotmail.es <juanbarrospazos@hotmail.es>
2022-07-02 14:04:36 +0200

    Juancho

--- Inclu/Inclu_Footer_01.php
+++ Inclu/Inclu_Footer_01.php
@@ -1,6 +1,6 @@
 </div>
 <div style="clear:both"></div>
-<div id="footer">&copy; Juan Barr&oacute;s Pazos 2017.</div>
+<div id="footer">&copy; Juan Barr&oacute;s Pazos 2014 / 2022.</div>
 </div>
 </body>
 </html>
